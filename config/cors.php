9e46e2a574ca455f341cec9547487f0acc7f8838
Drukumar01 <drukumar01@gmail.com>
2025-03-13 01:20:35 +0500

    Fixed API issue

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['https://zams-tech-ecom.vercel.app'],
+    'allowed_origins' => ['https://zams-tech-ecom-frontend-iuuq.vercel.app/'],
 
     'allowed_origins_patterns' => [],
 
